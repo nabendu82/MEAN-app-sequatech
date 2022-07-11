d733bf10b3e7e254dde3abe62c21e6f3cb9f6b6b
nabendu82 <nabendu.biswas@gmail.com>
2022-07-11 10:48:40 +0530

    Day 3

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
@@ -12,7 +13,9 @@ import { EmployeeComponent } from './employee/employee.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/employee/employee.component.ts
+++ frontend/src/app/employee/employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Employee } from '../employee.model';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -6,10 +9,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
+  empForm: FormGroup;
+  showModal: boolean = false;
+  editMode: boolean = false;
+  employees: Employee[];
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private empService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.getEmployees();
+    this.empForm = this.fb.group({
+      _id: [''],
+      name: ['Ex Nabendu Biswas', Validators.required],
+      position: ['Ex. ReactJS Trainer', Validators.required],
+      dept: ['Development']
+    })
+  }
+
+  getEmployees(){
+    this.empService.getEmployeeList().subscribe((res: Employee[]) => {
+      console.log(res);
+      this.employees = res;
+    })
+  }
+
+  onDeleteEmployee(id){
+    if(confirm('Do you want to delete this employee?')){
+      this.empService.deleteEmployee(id).subscribe(
+        res => {
+          console.log(res);
+          this.getEmployees();
+        }, err => {
+          console.log(err);
+        }
+      )
+    }
+  }
+
+  onEditEmployee(emp: Employee){
+    this.editMode = true;
+    this.showModal = true;
+    this.empForm.patchValue(emp);
+  }
+
+  onEmpSubmit(){
+    if(this.empForm.valid){
+      if(this.editMode){
+        this.empService.updateEmployee(this.empForm.value).subscribe(
+          res => {
+            console.log(res);
+            this.getEmployees();
+            this.onCloseModal();
+          },
+          err => console.log(err))
+      } else {
+        this.empService.addEmployee(this.empForm.value).subscribe(
+          res => {
+            console.log(res);
+            this.getEmployees();
+            this.onCloseModal();
+          },
+          err => console.log(err))
+      }
+    } else {
+      let key = Object.keys(this.empForm.controls);
+      key.filter(data => {
+        let control = this.empForm.controls[data];
+        if(control.errors != null){
+          control.markAsTouched()
+        }
+      })
+    }
+  }
+
+  onAddEmployee(){
+    this.showModal = true;
+  }
+
+  onCloseModal(){
+    this.showModal = false;
   }
 
 }
